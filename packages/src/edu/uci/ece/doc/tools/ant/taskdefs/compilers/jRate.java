b3b460d7c15d7688ad477ae85cdc167306020435
Krishna Raman <kraman@gmail.com>
2004-03-18 01:23:01 +0000

    Adding the jRate compiler ant task to RTZen
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@56 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/src/edu/uci/ece/doc/tools/ant/taskdefs/compilers/jRate.java
@@ -0,0 +1,354 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "Ant" and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package edu.uci.ece.doc.tools.ant.taskdefs.compilers;
+
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.Commandline;
+import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.apache.tools.ant.taskdefs.Execute;
+import java.io.File;
+
+/**
+<p>An Ant wrapper for the <a href="http://www.cs.wustl.edu/~corsaro/jRate/">jRate</a>
+compiler. It assumes that <code>jRate-gcj</code> is in the command path.</p>
+
+<p>The example below shows how this task could be used to build jRate's MemoryAreaDemo:</p>
+
+<pre>
+&lt;taskdef name="jrate" classname="edu.uci.ece.doc.tools.ant.taskdefs.compilers.jRate"/&gt;
+
+&lt;target name="compile"&gt;
+	&lt;jrate
+		optimizeLevel="2"
+		includeDirs="../../src"
+		libDirs="../../lib"
+		main="MemoryAreaDemo"
+		out="memoryDemo"&gt;
+		&lt;include name="*.java"/&gt;
+	&lt;/jrate&gt;
+&lt;/target&gt;
+</pre>
+
+<p>In addition to the attributes shown above, there are also the following:</p>
+
+<ul>
+<li>extdirs</li>
+<li>bootclasspath</li>
+<li>encoding</li>
+<li>debug</li>
+<li>classpath</li>
+<li>destdir</li>
+</ul>
+
+<p>See the setter method documentation below for more details on each attribute.</p>
+
+<p>Note: To specify source files to be compiled, you may use any of the tags
+supported by <a href="http://ant.apache.org/manual/CoreTypes/fileset.html">FileSet</a>.</p>
+
+<p>Requires Ant 1.6 or higher.</p>
+
+@author <a href="mailto:trevor@vocaro.com">Trevor Harmon</a>
+*/
+public class jRate extends MatchingTask {
+
+	private Path extdirs;
+	private Path bootclasspath;
+	private char optimizeLevel;
+	private String encoding;
+	private boolean debug;
+	private Path classpath;
+	private Path destdir;
+	private Path includeDirs;
+	private Path libDirs;
+	private String main;
+	private File out;
+	
+	/**
+	Sets the --extdirs parameter (only for compiling)
+	*/
+	public void setExtdirs(Path extdirs)
+	{
+		this.extdirs = extdirs;
+	}
+	
+	/**
+	Sets the --bootclasspath parameter (only for compiling)
+	*/
+	public void setBootclasspath(Path bootclasspath)
+	{
+		this.bootclasspath = bootclasspath;
+	}
+
+	/**
+	Sets the optimization level. The given character may be any of those
+	allowed by gcj's -O parameter: 1, 2, 3, or s. If not set, optimization
+	is turned off. (Affects both compiling and linking.)
+	*/
+	public void setOptimizeLevel(char optimizeLevel)
+	{
+		this.optimizeLevel = optimizeLevel;
+	}
+
+	/**
+	Sets the --encoding parameter (only for compiling)
+	*/
+	public void setEncoding(String encoding)
+	{
+		this.encoding = encoding;
+	}
+
+	/**
+	If true, -g is added to the command line (for compiling and linking)
+	*/
+	public void setDebug(boolean debug)
+	{
+		this.debug = debug;
+	}
+
+	/**
+	Sets the --classpath parameter (only for compiling)
+	*/
+	public void setClasspath(Path classpath)
+	{
+		this.classpath = classpath;
+	}
+
+	/**
+	Sets the -d parameter (only for compiling)
+	*/
+	public void setDestdir(Path destdir)
+	{
+		this.destdir = destdir;
+	}
+
+	/**
+	Sets the -I parameter for each of the given pathnames (only for compiling)
+	*/
+	public void setIncludeDirs(Path includeDirs)
+	{
+		this.includeDirs = includeDirs;
+	}
+	
+	/**
+	Sets the -L parameter for each of the given pathnames (only for linking)
+	*/
+	public void setLibDirs(Path libDirs)
+	{
+		this.libDirs = libDirs;
+	}
+	
+	/**
+	Sets the --main parameter (only for linking)
+	*/
+	public void setMain(String main)
+	{
+		this.main = main;
+	}
+	
+	/**
+	Sets the -o parameter (only for linking)
+	*/
+	public void setOut(File out)
+	{
+		this.out = out;
+	}
+	
+    /**
+    Performs a compile and link using the jRate compiler.
+	
+	@throws BuildException if there was an error running the compiler
+    */
+    public void execute() throws BuildException {
+
+        String[] files = getDirectoryScanner(getProject().getBaseDir()).getIncludedFiles();
+
+		log("Compiling and linking " + files.length + " source file"
+			+ (files.length == 1 ? "" : "s")
+			+ (destdir != null ? " to " + destdir : ""));
+		
+		for (int i = 0; i < files.length; i++) {
+			
+			// Prepend the base directory to the filename
+			String file = getProject().getBaseDir().getAbsolutePath() + System.getProperty("file.separator") + files[i];
+
+			if (!file.endsWith(".java"))
+			{
+				throw new BuildException("Expecting a file ending with \".java\" " +
+					"but got this instead: " + file);
+			}
+			
+			file = file.substring(0, file.lastIndexOf("java")); // Strip extension from filename
+	
+			Commandline cmd = getCompileCommand();
+			cmd.createArgument().setValue("-o");
+			cmd.createArgument().setValue(file + "o");
+			cmd.createArgument().setValue(file + "java");
+			
+			Execute.runCommand(this, cmd.getCommandline());
+		}
+
+		Execute.runCommand(this, getLinkCommand(files).getCommandline());
+    }
+
+	/**
+	Constructs and returns a Commandline object containing the first portion
+	(minus the input and output files) of a jRate compile command according to
+	the currently specified attributes.
+	*/
+    protected Commandline getCompileCommand() {
+        Commandline cmd = new Commandline();
+
+		cmd.setExecutable("jRate-gcj");
+		
+		if (includeDirs != null)
+		{
+			String[] dirs = includeDirs.list();
+			for (int i = 0; i < dirs.length; i++)
+			{
+				cmd.createArgument().setValue("-I" + dirs[i]);
+			}
+		}
+
+        if (bootclasspath != null) {
+			cmd.createArgument().setValue("--bootclasspath=" + bootclasspath);
+        }
+
+        if (classpath != null) {
+			cmd.createArgument().setValue("--classpath=" + classpath);
+        }
+
+        if (extdirs != null) {
+			cmd.createArgument().setValue("--extdirs=" + bootclasspath);
+        }
+
+        if (destdir != null) {
+            cmd.createArgument().setValue("-d");
+            cmd.createArgument().setPath(destdir);
+        }
+
+        if (encoding != null) {
+            cmd.createArgument().setValue("--encoding=" + encoding);
+        }
+
+        if (debug) {
+            cmd.createArgument().setValue("-g");
+        }
+
+        if (optimizeLevel != '\0') {
+            cmd.createArgument().setValue("-O" + optimizeLevel);
+        }
+
+		cmd.createArgument().setValue("-c");
+		
+        return cmd;
+    }
+
+	/**
+	Constructs and returns a complete Commandline object of a jRate link
+	command according to the currently specified attributes.
+	
+	@param files a list of files to be linked. These are assumed to be Java
+	files; their .java extensions will be replaced by .o for the command line.
+	*/
+    protected Commandline getLinkCommand(String[] files) {
+        Commandline cmd = new Commandline();
+
+		cmd.setExecutable("jRate-gcj");
+
+        if (optimizeLevel != '\0') {
+            cmd.createArgument().setValue("-O" + optimizeLevel);
+        }
+
+		if (libDirs != null)
+		{
+			String[] dirs = libDirs.list();
+			for (int i = 0; i < dirs.length; i++)
+			{
+				cmd.createArgument().setValue("-L" + dirs[i]);
+			}
+		}
+
+		if (main != null)
+		{
+			cmd.createArgument().setValue("--main=" + main);
+		}
+		
+		for (int i = 0; i < files.length; i++) {
+			
+			// Prepend the base directory to the filename
+			String file = getProject().getBaseDir().getAbsolutePath() + System.getProperty("file.separator") + files[i];
+
+			file = file.substring(0, file.lastIndexOf("java")); // Strip extension from filename
+	
+			cmd.createArgument().setValue(file + "o");
+		}
+			
+		if (out != null)
+		{
+			cmd.createArgument().setValue("-o");
+			cmd.createArgument().setFile(out);
+		}
+
+		cmd.createArgument().setValue("-ljRateRT");
+		cmd.createArgument().setValue("-ljRate");
+		cmd.createArgument().setValue("-ljRateCore");
+		cmd.createArgument().setValue("-lrt");
+		
+		return cmd;		
+	}
+}
