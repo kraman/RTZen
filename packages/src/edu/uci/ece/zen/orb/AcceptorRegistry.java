6a9d0bf984bb38cddc84f291b66cd51f9a6faf91
Mark Panahi <mpanahi@doc.ece.uci.edu>
2004-04-23 19:20:06 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@176 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/src/edu/uci/ece/zen/orb/AcceptorRegistry.java
+++ packages/src/edu/uci/ece/zen/orb/AcceptorRegistry.java
@@ -10,33 +10,41 @@ public class AcceptorRegistry{
     private Vector list = new Vector();
     private TaggedProfile[] tpList;
 
-    public TaggedProfile[] getProfiles(){
+    public TaggedProfile[] getProfiles(byte [] objKey, int objKeyLength){
         tpList = new TaggedProfile[list.size()];
+
+        byte [] tempOKey = new byte[objKeyLength];
+
+        //kludge: assuming objKey.length >= objKeyLength
+        System.arraycopy(objKey, 0, tempOKey, 0, objKeyLength);
 
         for(int i = 0; i < list.size(); ++i){
             ScopedMemory sm = (ScopedMemory)(list.get(i));
-            sm.enter(new ARRunnable(i,RealtimeThread.getCurrentMemoryArea()));
+            sm.enter(new ARRunnable(i, tempOKey, RealtimeThread.getCurrentMemoryArea()));
+
         }
 
         return tpList;
     }
-
+    //
     class ARRunnable implements Runnable{
 
         private int index;
         MemoryArea ma;
+        byte[] objKey;
 
-        public ARRunnable(int ind, MemoryArea ma){
+        public ARRunnable(int ind, byte [] objKey, MemoryArea ma){
             index = ind;
             this.ma = ma;
-
+            this.objKey = objKey;
         }
 
         public void run(){
             Acceptor acc = (Acceptor) ((ScopedMemory)RealtimeThread.getCurrentMemoryArea()).getPortal();
-            tpList[index] = acc.getProfile((byte)1, (byte)0, ma);
+            tpList[index] = acc.getProfile((byte)1, (byte)0, objKey, ma);
         }
     }
+
 }
 
 
