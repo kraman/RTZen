4a234d0158103603f66b033e07728463c3b30494
Krishna Raman <kraman@gmail.com>
2004-06-04 19:13:27 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@351 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/src/edu/uci/ece/zen/poa/mechanism/ActivationStrategy.java
+++ packages/src/edu/uci/ece/zen/poa/mechanism/ActivationStrategy.java
@@ -1,12 +1,8 @@
-/* --------------------------------------------------------------------------*
- * $Id: ActivationStrategy.java,v 1.2 2004/03/11 19:31:37 nshankar Exp $
- *--------------------------------------------------------------------------*/
-
 package edu.uci.ece.zen.poa.mechanism;
 
-import edu.uci.ece.zen.poa.POAPolicyFactory;
-import edu.uci.ece.zen.poa.Util;
-import edu.uci.ece.zen.sys.ZenProperties;
+import edu.uci.ece.zen.utils.ZenProperties;
+import org.omg.CORBA.Policy;
+import org.omg.CORBA.IntHolder;
 
 /**
  * The class <code>ActivationStrategy</code> creates either a Implicit/
@@ -16,17 +12,14 @@ import edu.uci.ece.zen.sys.ZenProperties;
  * @version 1.0
  */
 public abstract class ActivationStrategy {
-    // --- Strategy names ---
-    protected static final String implicit = "poa.implicitActivation";
-    protected static final String explicit = "poa.explicitActivation";
 
-    // --- Initialization Code ----
-    static {
-        ActivationStrategy.implicitActivation = (ImplicitActivationStrategy)
-                POAPolicyFactory.createPolicy(ZenProperties.getProperty(ActivationStrategy.implicit));
-        //ActivationStrategy.noImplicitActivation = (ExplicitActivationStrategy)
-        //        POAPolicyFactory.createPolicy(ZenProperties.getProperty(ActivationStrategy.explicit));
-    }
+    public static final int NoException = 0;
+    public static final int InvalidPolicyException = 1;
+
+    // --- Type Values ---
+    public static int IMPLICIT_ACTIVATION = 0;
+    public static int EXPLICIT_ACTIVATION = 1;
+
 
     /**
      *
@@ -35,35 +28,32 @@ public abstract class ActivationStrategy {
      * @param retentionStrategy ServantRetentionStrategy
      * @return ActivationStrategy
      */
-    public static ActivationStrategy init(org.omg.CORBA.Policy[] policy,
-            IdAssignmentStrategy assignmentStrategy,
-            ServantRetentionStrategy retentionStrategy)
-        throws org.omg.PortableServer.POAPackage.InvalidPolicy {
+    public static ActivationStrategy init(Policy[] policy, IdAssignmentStrategy assignmentStrategy,
+             ServantRetentionStrategy retentionStrategy , IntHolder exceptionValue ){
 
-        //if (Util.useImplicitActivationPolicy(policy)) {
-           // try {
-                // Check if the other policies are Retain and System Id
-              //  retentionStrategy.validate(ServantRetentionStrategy.RETAIN);
-                //assignmentStrategy.validate(IdAssignmentStrategy.SYSTEM_ID);
+        exceptionValue.value = ActivationStrategy.NoException;
 
-                return ActivationStrategy.implicitActivation;
-            //} catch (Exception ex) {
-               // throw new org.omg.PortableServer.POAPackage.InvalidPolicy();
-            //}
-        //}
+        if (PolicyUtils.useImplicitActivationPolicy(policy)) {
+            // Check if the other policies are Retain and System Id
+            retentionStrategy.validate(ServantRetentionStrategy.RETAIN,exceptionValue );
 
-        //return ActivationStrategy.noImplicitActivation;
+            if( exceptionValue.value != ServantRetentionStrategy.NoException ){
+                exceptionValue.value = ActivationStrategy.InvalidPolicyException;
+                return null;
+            }
+
+            assignmentStrategy.validate(IdAssignmentStrategy.SYSTEM_ID);
+            if( exceptionValue.value != IdAssignmentStrategy.NoException ){
+                exceptionValue.value = ActivationStrategy.InvalidPolicyException;
+                return null;
+            }
+
+            return new ImplicitActivationStrategy();
+        }else{
+            return new ExplicitActivationStrategy();
+        }
     }
 
-    public abstract boolean validate(int name);
-
-    // --- Type Values ---
-    public static int IMPLICIT_ACTIVATION = 0;
-    public static int EXPLICIT_ACTIVATION = 1;
-
-    // --- Singleton references ---
-    private static ImplicitActivationStrategy implicitActivation;
-    //private static ExplicitActivationStrategy noImplicitActivation;
-
+    public abstract void validate(int name , IntHolder exceptionValue);
 }
 
--- packages/src/edu/uci/ece/zen/poa/mechanism/ImplicitActivationStrategy.java
+++ packages/src/edu/uci/ece/zen/poa/mechanism/ImplicitActivationStrategy.java
@@ -1,23 +1,20 @@
-/* --------------------------------------------------------------------------*
- * $Id: ImplicitActivationStrategy.java,v 1.1 2003/11/26 22:28:55 nshankar Exp $
- *--------------------------------------------------------------------------*/
-
 package edu.uci.ece.zen.poa.mechanism;
 
+import org.omg.CORBA.IntHolder;
 
 public final class ImplicitActivationStrategy extends ActivationStrategy {
 
-   /**
-    * Check if the strategy value is same as this strategys value. Usually
-    * used to check if two strategies are the same
-    * @param name Strategies integer value
-    * @return boolean true if same, else false.
-    */
- public boolean validate(int name) {
+    /**
+     * Check if the strategy value is same as this strategys value. Usually
+     * used to check if two strategies are the same
+     * @param name Strategies integer value
+     * @return boolean true if same, else false.
+     */
+    public void validate(int name, IntHolder exceptionValue ) {
         if (ActivationStrategy.IMPLICIT_ACTIVATION == name) {
-            return true;
+            exceptionValue.value = ActivationStrategy.NoException;
         } else {
-            return false;
+            exceptionValue.value = ActivationStrategy.InvalidPolicyException;
         }
     }
 
