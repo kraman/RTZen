c33a65f88063187869dc5781cedb62c47295b053
Mark Panahi <mpanahi@doc.ece.uci.edu>
2004-06-06 20:10:40 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@358 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/demo/rtcorba/serverDeclared/timesys.sh
@@ -0,0 +1,1 @@
+tjvm -Xverify:all -Ximmortal100M -Xms200M -Djava.class.path=/project/workarea02/mpanahi/RTZen/classes/ -Xbootclasspath=/opt/timesys/rtsj-ri/lib/foundation.jar demo.rtcorba.serverDeclared.Client iorfile1 iorfile2
