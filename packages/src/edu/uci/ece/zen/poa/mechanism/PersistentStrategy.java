907634f91b4e488e96c4b3bbed6bdce83c33a5a4
Peter Martini <pmartini@doc.ece.uci.edu>
2004-06-07 20:32:09 +0000

    Problem with the poa_path and other in that they are byte[] and need FSTring
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@373 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/src/edu/uci/ece/zen/poa/mechanism/PersistentStrategy.java
+++ packages/src/edu/uci/ece/zen/poa/mechanism/PersistentStrategy.java
@@ -1,12 +1,8 @@
-/* --------------------------------------------------------------------------*
- * $Id: PersistentStrategy.java,v 1.5 2003/08/05 23:37:28 nshankar Exp $
- *--------------------------------------------------------------------------*/
-
-
 package edu.uci.ece.zen.poa.mechanism;
 
 
 import edu.uci.ece.zen.poa.ActiveDemuxLoc;
+import org.omg.CORBA.IntHolder;
 
 
 public final class PersistentStrategy extends LifespanStrategy {
@@ -14,17 +10,16 @@ public final class PersistentStrategy extends LifespanStrategy {
     public PersistentStrategy() {
         this.value = -1;
 
-        this.timeStamp = new byte[8];
-        int nextFreeByte = 0;
+        timeStamp.reset();
 
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 56) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 48) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 40) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 32) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 24) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 16) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) ((value >>> 8) & 0xFF);
-        timeStamp[nextFreeByte++] = (byte) (value & 0xFF);
+        timeStamp.append((byte) ((value >>> 56) & 0xFF));
+        timeStamp.append((byte) ((value >>> 48) & 0xFF));
+        timeStamp.append((byte) ((value >>> 40) & 0xFF));
+        timeStamp.append((byte) ((value >>> 32) & 0xFF));
+        timeStamp.append((byte) ((value >>> 24) & 0xFF));
+        timeStamp.append((byte) ((value >>> 16) & 0xFF));
+        timeStamp.append((byte) ((value >>> 8) & 0xFF));
+        timeStamp.append((byte) (value & 0xFF));
     }
 
     /**
@@ -43,12 +38,12 @@ public final class PersistentStrategy extends LifespanStrategy {
     * @param loc active demux location
     * @return edu.uci.ece.zen.poa.ObjectKey corresponding object key
     */
-    public edu.uci.ece.zen.poa.ObjectKey create(String path_name,
-                                                byte[] oid,
-                                                ActiveDemuxLoc loc) {
-
+    public void  create(FString path_name,
+                                                FString oid,
+                                                ActiveDemuxLoc loc, FString okey_out) {
+    // I don't know what you want here, but the loc is a byte[] and needs to be a FString.  Maybe create one and append?
         return IdNoHintStrategy.create(PersistentStrategy.prefix, path_name,
-                this.timeStamp, oid, loc.marshall());
+                this.timeStamp, oid, loc.marshall(), okey_out);
     }
 
    /**
@@ -59,11 +54,12 @@ public final class PersistentStrategy extends LifespanStrategy {
     * @param servLoc demxu location of the servant
     * @return edu.uci.ece.zen.poa.ObjectKey
     */
-    public edu.uci.ece.zen.poa.ObjectKey create(String path_name,
-                                                byte[] oid,
+    public edu.uci.ece.zen.poa.ObjectKey create(FString path_name,
+                                                FString oid,
                                                 ActiveDemuxLoc poaLoc,
                                                 ActiveDemuxLoc servLoc) {
 
+        // Same as above
         return IdHintStrategy.create(PersistentStrategy.prefix, path_name,
                 this.timeStamp, oid, poaLoc.marshall(), servLoc.marshall());
 
@@ -74,17 +70,17 @@ public final class PersistentStrategy extends LifespanStrategy {
     * @param ok ObjectKey
     * @throws org.omg.CORBA.OBJECT_NOT_EXIST
     */
-    public void validate(edu.uci.ece.zen.poa.ObjectKey ok) throws
-                org.omg.CORBA.OBJECT_NOT_EXIST {
+    public void validate(edu.uci.ece.zen.poa.ObjectKey ok, IntHolder exceptionValue
+    {
+        exceptionValue.value = POARunnable.NoException;
 
         if (!ok.isPersistent() && !ok.compareTimeStamps(this.timeStamp)) {
-            throw new org.omg.CORBA.OBJECT_NOT_EXIST(2,
-                    org.omg.CORBA.CompletionStatus.COMPLETED_NO);
+            exceptionValue.value = POARunnable.ObjNotExistException;
         }
     }
 
     protected long value;
-    protected byte[] timeStamp;
+    protected FString timeStamp;
     // --the character that has to be prepened
     protected static char prefix = 'P';
 }
