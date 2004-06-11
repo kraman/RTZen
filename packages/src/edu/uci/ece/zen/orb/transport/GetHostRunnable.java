379b026932b3568d8e09a726021ca4d490d61a70
Krishna Raman <kraman@gmail.com>
2004-06-11 05:36:50 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@401 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/src/edu/uci/ece/zen/orb/transport/GetHostRunnable.java
@@ -0,0 +1,23 @@
+package edu.uci.ece.zen.orb.transport;
+
+import javax.realtime.*;
+import edu.uci.ece.zen.orb.*;
+import edu.uci.ece.zen.utils.*;
+import java.net.*;
+
+class GetHostRunnable implements Runnable{
+    public InetAddress inetaddr;
+    public String host;
+
+    public GetHostRunnable( String host ){
+        this.host = host;
+    }
+
+    public void run(){
+        try{
+            inetaddr = InetAddress.getByName( host );
+        }catch( Exception e ){
+            e.printStackTrace();
+        }
+    }
+}
