bb1f36b8b23cd7e42cf19e530e83236d3acfa7b4
Peter Martini <pmartini@doc.ece.uci.edu>
2004-06-21 21:59:38 +0000

    Fixed cool script
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@421 bfff1265-3dd4-0310-90a8-ce2828190deb

--- bin/timesys-env.sh
+++ bin/timesys-env.sh
@@ -1,5 +1,5 @@
-################# TIMESYS
+#TIMESYS
 
-export LD_LIBRARY_PATH /opt/timesys/rtsj-ri/lib/:/opt/timesys/rtsj-ri/pthreadrt:$LD_LIBRARY_PATH
-export LD_ASSUME_KERNEL 2.4.1
+export LD_LIBRARY_PATH=/opt/timesys/rtsj-ri/lib/:/opt/timesys/rtsj-ri/pthreadrt:$LD_LIBRARY_PATH
+export LD_ASSUME_KERNEL=2.4.1
 
