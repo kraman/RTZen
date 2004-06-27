a10d686eddb3d2cd10280e6ed0bc499378bf2595
Gunar Schirne <hschirne@doc.ece.uci.edu>
2004-06-27 21:19:16 +0000

    update
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@486 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/perf/jitter/gen.m
+++ packages/perf/jitter/gen.m
@@ -1,21 +1,25 @@
-clc;
-clear;
-close all;
+
+fNameBase = "timeRecords.1.3.2.";
+
+%clc;
+%clear;
+%close all;
 
 noOfBins = 999;
 difference = 2;
 stepSmall = 0.2;
+stepNotThere = 0.00000000001;
 
 barWidth = 0.1;
 
 xIndexArray = [4 32 128 512];
 
-
+%xticks(2);
 for xIndex = 2:difference:8
 
 	% clear fileName, hiss, pdfArray;
 
-	fileName = strcat('timeRecords.1.3.2.', num2str(round((xIndexArray([xIndex/difference])))), '.txt');
+	fileName = strcat(fNameBase, num2str(round((xIndexArray([xIndex/difference])))), '.txt');
         s = load(fileName);
 	s = diff(s(:, 3));
 
@@ -32,23 +36,29 @@ for xIndex = 2:difference:8
 	pdfArray = (minS:stepSize:(maxS - stepSize));
 
 	hold on;
-	semilogy([xIndex xIndex], [minS maxS], 'r');
+    % draw the line from min to max
+	semilogy([xIndex - stepNotThere xIndex + stepNotThere], [minS maxS], '-3;;');
 	hold on;
-        % semilogy the historam 
-	%semilogy((hiss + xIndex) , pdfArray);
-	semilogy((hiss + xIndex) , pdfArray);
+    % splot the historam 
+	%semilogy((hiss + xIndex) , pdfArray, '-1;;');
+	semilogy((hiss + xIndex) , pdfArray, '-3;;');
 	hold on;
-
-	semilogy(xIndex , averageS, 'bo');
+    % place the dot for the average
+	semilogy(xIndex , averageS, '1o;;');
 	hold on;
-
-        semilogy([xIndex - barWidth xIndex + barWidth], [minS minS]);
+    % minimum bar
+    semilogy([xIndex - barWidth xIndex + barWidth], [minS minS],'-3;;');
 	hold on;
-	semilogy([xIndex - barWidth xIndex + barWidth], [maxS maxS]);
+    % maximum bar
+	semilogy([xIndex - barWidth xIndex + barWidth], [maxS maxS],'-3;;');
 
 end
 
 title('Roundtrip Latency/Jitter, RTZen/TimeSys, Single Host Emulab');
 ylabel('Roundtrip Latency [seconds] ');
 xlabel('Message size [bytes] (4 32 128 512)');
-set(gca, 'xtick', xIndexArray);
+%set(gca, 'xtick', xIndexArray);
+
+gset term postscript eps color;
+gset output "timeRecords.eps";
+replot;
