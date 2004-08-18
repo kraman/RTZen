1d1f219a2ee4cdaf9e4db15568bde0dfe226f76d
Krishna Raman <kraman@gmail.com>
2004-08-18 23:09:15 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@646 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/hello/timesys.sh
+++
@@ -1,1 +0,0 @@
-tjvm -Xverify:all -Ximmortal100M -Xms200M -Djava.class.path=$HOME/RTZen/classes/ -Xbootclasspath=/opt/timesys/rtsj-ri/lib/foundation.jar -Djava.library.path=$LD_LIBRARY_PATH demo.hello.Client
--- packages/demo/hello/timesys_server.sh
+++ packages/demo/hello/timesys_server.sh
@@ -1,1 +1,1 @@
-tjvm -Xverify:all -Ximmortal100M -Xms200M -Djava.class.path=$HOME/RTZen/classes/ -Xbootclasspath=/opt/timesys/rtsj-ri/lib/foundation.jar -Djava.library.path=$LD_LIBRARY_PATH demo.hello.Server
+/opt/timesys/rtsj_ri_1.1/bin/tjvm -Xverify:all -Ximmortal100M -Xms200M -Djava.class.path=$HOME/RTZen/classes/ -Xbootclasspath=/opt/timesys/rtsj-ri/lib/foundation.jar -Djava.library.path=$LD_LIBRARY_PATH demo.hello.Server
