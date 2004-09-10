0628775ea9eaa69d32404c6488b9f98c1170a996
Krishna Raman <kraman@gmail.com>
2004-09-10 04:57:11 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@792 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/src/edu/uci/ece/zen/orb/MessageComposerRunnable.java
@@ -0,0 +1,102 @@
+package edu.uci.ece.zen.orb;
+
+import javax.realtime.*;
+import edu.uci.ece.zen.utils.*;
+
+public class MessageComposerRunnable implements Runnable {
+    ClientRequest clr;
+
+    CDRInputStream reply;
+
+    boolean success;
+
+    private static MessageComposerRunnable inst;
+
+    public MessageComposerRunnable(){}
+
+    public static MessageComposerRunnable instance() {
+        if (inst == null) {
+            try {
+                inst = (MessageComposerRunnable) ImmortalMemory.instance()
+                        .newInstance(MessageComposerRunnable.class);
+            } catch (Exception e) {
+                ZenProperties.logger.log(Logger.FATAL, MessageComposerRunnable.class, "instance", e);
+            }
+        }
+        return inst;
+    }
+
+    /**
+     * Client upcall:
+     * <p>
+     * <b>Client scope </b> --ex in --&gt; ORB parent scope --&gt; ORB scope
+     * --&gt; Message scope/Waiter region --&gt; Transport scope
+     * </p>
+     */
+    public void init(ClientRequest clr) {
+        this.clr = clr;
+    }
+
+    /**
+     * Client upcall:
+     * <p>
+     * Client scope --ex in --&gt; ORB parent scope --&gt; ORB scope --&gt;
+     * <b>Message scope/Waiter region </b> --&gt; Transport scope
+     * </p>
+     */
+    private int statCount = 0;
+    public void run() {
+            if (statCount % 100 == 0) {
+                edu.uci.ece.zen.utils.Logger.printMemStats(6);
+            }
+            statCount++;
+
+        //setup waiting straterg
+        WaitingStrategy waitingStrategy = null;
+        if (clr.responseExpected) {
+            //waitingStrategy = TwoWayWaitingStrategy.instance();//new
+            // TwoWayWaitingStrategy();
+            //TODO: Krishna, make sure this is correct, had to do this to solve
+            // mem leak
+            waitingStrategy = (WaitingStrategy) (((ScopedMemory) RealtimeThread
+                    .getCurrentMemoryArea()).getPortal());
+            if (waitingStrategy == null) {
+                waitingStrategy = new TwoWayWaitingStrategy();
+                ((ScopedMemory) RealtimeThread.getCurrentMemoryArea())
+                        .setPortal(waitingStrategy);
+            }
+            clr.registerWaiter();
+        }
+
+        ExecuteInRunnable eir = clr.orb.getEIR();//new ExecuteInRunnable();
+        SendMessageRunnable smr = SendMessageRunnable.instance();
+        smr.init(clr.transportScope);
+        smr.init(clr.out.getBuffer());
+        eir.init(smr, clr.transportScope);
+        success = true;
+        try {
+            ZenProperties.logger.log("MCR run 1");
+            clr.orb.orbImplRegion.executeInArea(eir);
+        } catch (Exception e) {
+            ZenProperties.logger.log(Logger.SEVERE,
+                    getClass(), "run",
+                    "Could not sent message on transport", e);
+            clr.releaseWaiter();
+            waitingStrategy = null;
+            success = false;
+        } finally {
+            clr.out.free();
+        }
+
+        if (waitingStrategy != null) {
+            reply = waitingStrategy.waitForReply();
+            clr.releaseWaiter();
+        }
+        clr.orb.freeEIR( eir );
+    }
+
+    public CDRInputStream getReply() {
+        return reply;
+    }
+}
+
