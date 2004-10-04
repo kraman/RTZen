8cecbe145efdef66cdcef817195fbe00564ae691
Krishna Raman <kraman@gmail.com>
2004-10-04 21:18:10 +0000

    Changed the environment setting in sh
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1236 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/perf/jitter/OVMServer_INT/compile_server_interpreter.sh
+++ packages/perf/jitter/OVMServer_INT/compile_server_interpreter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export OVMHOME=/home/yuez/OpenVM
-export RTZENHOME=/home/yuez/RTZen
+export OVMHOME=/groups/pces/uav_oep/exp/RTZEN/tools/OpenVM
+export RTZENHOME=/users/kraman/RTZen
 
 $OVMHOME/OpenVM_bin/bin/gen-ovm -engine=interpreter -threads=RealtimeJVM -model=MostlyCopyingRegions-B_M_F_H  -app-methods-live -classpath=$RTZENHOME/lib/RTZen.jar:$RTZENHOME/lib/perf_world.jar -main=perf.jitter.Server
 #
