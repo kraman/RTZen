b1638fbbcf459e283f0b29681dcd44ff0a17e561
Trevor Harmon <trevor@vocaro.com>
2004-10-06 02:06:45 +0000

    Merging with trunk
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/branches/serial-transport@1267 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/perf/jitter/OVMClient_INT/compile.sh
@@ -0,0 +1,6 @@
+ 
+export OVMHOME=/groups/pces/uav_oep/exp/RTZEN/tools/OpenVM
+export RTZENHOME=/users/kraman/RTZen
+
+$OVMHOME/OpenVM_bin/bin/gen-ovm -engine=interpreter -immortal-size=100m -threads=RealtimeJVM -model=MostlyCopyingRegions-B_M_F_H  -app-methods-live -reflective-method-trace=methods -reflective-class-trace=classes -classpath=$RTZENHOME/lib/RTZen.jar:$RTZENHOME/lib/perf_world.jar -main=perf.jitter.OVMClient
+
