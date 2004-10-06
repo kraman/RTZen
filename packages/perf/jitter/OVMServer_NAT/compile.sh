5e0c1c88cb30f30a00e51d2a0f56023854ebb531
Yue Zhang <yuez@doc.ece.uci.edu>
2004-10-06 16:25:22 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1285 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/perf/jitter/OVMServer_NAT/compile.sh
+++ packages/perf/jitter/OVMServer_NAT/compile.sh
@@ -1,5 +1,5 @@
-cp ../OVMServer_INT/methods .
-cp ../OVMServer_INT/classes .
+#cp ../OVMServer_INT/methods .
+#cp ../OVMServer_INT/classes .
 
-$OVM/OpenVM_bin/bin/gen-ovm -engine=j2c -immortal-size=100m -threads=RealtimeJVM -model=MostlyCopyingRegions-B_M_F_H  -ud-reflective-method=@methods -ud-reflective-class=@classes -classpath=$RTZEN_OVM/lib/RTZen.jar:$RTZEN_OVM/lib/perf_world.jar -main=perf.jitter.Server
+$OVM/OpenVM_bin/bin/gen-ovm -engine=j2c -immortal-size=100m -threads=RealtimeJVM -model=MostlyCopyingRegions-B_M_F_H  -ud-reflective-methods=@methods -ud-reflective-classes=@classes -classpath=$RTZEN_OVM/lib/RTZen.jar:$RTZEN_OVM/lib/perf_world.jar -main=perf.jitter.Server
 
