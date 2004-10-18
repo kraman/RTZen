0b3dfb6094e2563e11b1d845be6977469e12762f
Yue Zhang <yuez@doc.ece.uci.edu>
2004-10-18 03:27:26 +0000

    Set the IO configuration and build optimization options
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1405 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/perf/jitter/OVMClient_NAT/compile.sh
+++ packages/perf/jitter/OVMClient_NAT/compile.sh
@@ -1,5 +1,5 @@
 cp ../OVMClient_INT/methods .
 cp ../OVMClient_INT/classes .
 
-$OVM/OpenVM_bin/bin/gen-ovm -engine=j2c -immortal-size=100m -threads=RealtimeJVM -model=MostlyCopyingRegions-B_M_F_H  -ud-reflective-methods=@methods -ud-reflective-classes=@classes -classpath=$RTZEN_OVM/lib/RTZen.jar:$RTZEN_OVM/lib/perf_world.jar -main=perf.jitter.OVMClient
+$OVM/OpenVM_bin/bin/gen-ovm -engine=j2c -immortal-size=100m -threads=RealtimeJVM -model=MostlyCopyingRegions-B_M_F_H  -opt=run -io=SIGIOSockets_PollingOther -ud-reflective-methods=@methods -ud-reflective-classes=@classes -classpath=$RTZEN_OVM/lib/RTZen.jar:$RTZEN_OVM/lib/perf_world.jar -main=perf.jitter.OVMClient
 
