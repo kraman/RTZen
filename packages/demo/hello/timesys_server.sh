f2e5c8ae1d5c7ede836b0e902fb5141bb5f4006e
Juan Colmenares <juancol@doc.ece.uci.edu>
2005-01-25 20:14:08 +0000

    modifications in hello example's shell scripts
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/branches/poa-and-policies@1814 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/hello/timesys_server.sh
+++ packages/demo/hello/timesys_server.sh
@@ -1,2 +1,9 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/timesys/rtsj_ri_1.1/pthreadrt/
-/opt/timesys/rtsj_ri_1.1/bin/tjvm -Xverify:all -Ximmortal100M -Xms200M -Djava.class.path=$HOME/RTZen/classes/ -Xbootclasspath=/opt/timesys/rtsj-ri/lib/foundation.jar -Djava.library.path=$LD_LIBRARY_PATH demo.hello.Server
+
+export LD_ASSUME_KERNEL=2.4.1;
+
+RTSJ_DIR=/opt/timesys/rtsj-ri
+HOME=/home/juancol/SVN-REPOSITORY/RTZen/branches/poa-and-policies
+
+$RTSJ_DIR/bin/tjvm -Xverify:all -Ximmortal100M -Xms200M -Djava.class.path=$HOME/classes/ -Xbootclasspath=/opt/timesys/rtsj-ri/lib/foundation.jar -Djava.library.path=$LD_LIBRARY_PATH demo.hello.Server
+
