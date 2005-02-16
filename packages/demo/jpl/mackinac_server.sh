efa96a90de9433709aab55b9247062c96d7a23f2
Yue Zhang <yuez@doc.ece.uci.edu>
2005-02-16 11:59:16 +0000

    Changed the running script back to interpreted mode
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1936 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/jpl/mackinac_server.sh
+++ packages/demo/jpl/mackinac_server.sh
@@ -1,6 +1,4 @@
 export RTZEN_HOME=/home/yuez/RTZen_test
-export PATH=$MACKINAC_HOME/bin:$PATH
-#java -Xbootclasspath/p:/$RTZEN_HOME/lib/RTZen.jar:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=100M -XX:ScopedSize=100M -XX:+VMStackTraceAtRTSJException -XX:+RTSJTraceAccessChecks -XX:+RTSJTraceAssignmentChecks -XX:+RTSJAccessChecksDebug -XX:+RTSJAssignmentChecksDebug -XX:+RTSJIgnoreThrowBoundaryError -classpath $RTZEN_HOME/classes demo.poa2.Server
 
-java -Xbootclasspath/p:/$RTZEN_HOME/lib/RTZen.jar:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
+java -Xint -Xbootclasspath/p:/$RTZEN_HOME/lib/RTZen.jar:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
 
