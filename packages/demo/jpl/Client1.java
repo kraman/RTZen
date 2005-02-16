90ed637e7592ccdbeec8e334bacdcd45dbc6aa08
Juan Colmenares <juancol@doc.ece.uci.edu>
2005-02-16 01:08:17 +0000

    This is a initial version of the demo for JPL.
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1919 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/demo/jpl/Client1.java
@@ -0,0 +1,117 @@
+package demo.jpl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.realtime.ImmortalMemory;
+import javax.realtime.RealtimeThread;
+
+import org.omg.CORBA.ORB;
+
+import edu.uci.ece.zen.utils.Logger;
+
+/**
+ * This class implements a simple CORBA client
+ * @version 1.0
+ */
+public class Client1 extends RealtimeThread
+{
+    private static final int A_SECOND = 1000;
+    private static final int INITIAL_SLEEP = A_SECOND;
+    private static final int REQUEST_SLEEP = 100;
+    public static final int RUN_NUM = 50000;
+    public static final int ARRAY_SIZE = 200;
+
+    static int[] array1 = new int[ARRAY_SIZE];
+
+    static
+    {
+        for (int i = 0; i < array1.length; i++)
+        {
+            array1[i] = i + 10;
+        }
+    }
+
+    static String fileName;
+
+    static int id;
+    
+    /**
+     * Main function.
+     */
+    public static void main(String[] args)
+    {
+        try
+        {
+            if (args.length < 2)
+            {
+                System.out.println("Usage: Client1 <iorfile> <#id>");
+            }
+            
+            fileName = args[0];
+            id = Integer.parseInt(args[1]);
+            System.out.println("==============Creating RT Thread in client ==============");
+            RealtimeThread rt = (Client1) ImmortalMemory.instance().newInstance(Client1.class);
+            System.out.println("==============Starting RT Thread in client==============");
+            rt.start();            
+        }
+        catch (Throwable e)
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+    
+    public void run()
+    {
+        try
+        {
+            System.out.println("==============Calling ORB Init in client==============");
+            ORB orb = ORB.init((String[]) null, null);
+            System.out.println("==============ORB Init complete in client==============");
+            String ior = "";
+            File iorfile = new File(fileName);
+            BufferedReader br = new BufferedReader(new FileReader(iorfile));
+            ior = br.readLine();
+            System.out.println("==============IOR read==============");
+            org.omg.CORBA.Object object = orb.string_to_object(ior);
+            System.out.println("==============Trying to establish connection==============");
+            final HelloWorld server = HelloWorldHelper.unchecked_narrow(object);
+
+            sleep(INITIAL_SLEEP);
+
+            System.out.println("==============Performance benchmark==============");
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < RUN_NUM; i++)
+            {
+                {
+                    System.out.print("# ");
+                    System.out.println(server.getMessage(id, array1));
+                    sleep(REQUEST_SLEEP);
+                }
+
+                sleep(REQUEST_SLEEP);
+
+                if (i != 0 && i % 500 == 0)
+                {
+                    Logger.write(i); 
+                    Logger.writeln();
+                }
+            }
+            long end = System.currentTimeMillis();
+            System.err.println((double) RUN_NUM / ((end - start) / 1000.0));
+
+            System.exit(0);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+
+    
+}
---
+++ packages/demo/jpl/HelloWorldImpl.java
@@ -0,0 +1,33 @@
+package demo.jpl;
+
+/**
+ * This class implements the simple Hello World server.
+ * @version 1.0
+ */
+
+public class HelloWorldImpl extends HelloWorldPOA
+{
+    private static int ITER = 10000;
+    private int val;
+    
+    public HelloWorldImpl(int value)
+    {
+       this.val = value;    
+    }
+    
+    /**
+     * Gets a message from the Hello World Server.
+     */
+    public int getMessage(int id, int[] array)
+    {
+        for (int i = 0; i < this.val * ITER; i++)
+        {
+            int j = i % array.length; 
+            array[j] = id;
+        }
+        
+        System.out.println(id + " Request got here ! ...now sending it back. " + array[0]);
+        return id;
+    }
+}
+
