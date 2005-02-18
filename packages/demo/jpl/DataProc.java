464cd53f2528621fb05752646abe50d38b4c4ea4
Krishna Raman <kraman@gmail.com>
2005-02-18 05:44:38 +0000

    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1957 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/jpl/DataProc.java
+++ packages/demo/jpl/DataProc.java
@@ -7,45 +7,52 @@ import java.util.Vector;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 class DataProc{
-    /*
-    Vector v1 = new Vector();
-    Vector v2 = new Vector();
-    Vector v1Proc = new Vector();
-    Vector v2Proc = new Vector();
     public static void main(String[] args){
         try{
             BufferedReader br = new BufferedReader(new FileReader("timeRecords.txt"));
             String s;
             int id;
 
+            Vector v1 = new Vector();
+            Vector v2 = new Vector();
+            Vector v1Proc = new Vector();
+            Vector v2Proc = new Vector();
+
+
             while((s = br.readLine())!=null){
                 StringTokenizer st = new StringTokenizer(s, ",");
-                s.nextToken();
-                id = Integer.parseInt(s.nextToken());
+                st.nextToken();
+                id = Integer.parseInt(st.nextToken());
                 switch(id){
                     case 20: break;
-                    case 21: v1.addElement(Double.parseDouble(s.nextToken()))
+                    case 21: 
+                             Double db1 = new Double(Double.parseDouble(st.nextToken()));
+                             v1.addElement(db1);
                              break;
-                    case 22: v2.addElement(Double.parseDouble(s.nextToken()));break;
+                    case 22: 
+                             Double db2 = new Double(Double.parseDouble(st.nextToken()));
+                             v2.addElement(db2);
+                             break;
                     default:System.err.println("Unrecogzied position id "+id);
                             System.exit(-1);
                 }
             }
             for(int i = 0; i < v2.size();i++){
                 if(i%2==1){
-                    double d1 = (Double)v2.elementAt(i).doubleValue();
-                    double d2 = (Double)v2.elementAt(i-1).doubleValue();
-                    v2Proc.addElement(d1-d2);
+                    double d1 = ((Double)v2.elementAt(i)).doubleValue();
+                    double d2 = ((Double)v2.elementAt(i-1)).doubleValue();
+                    v2Proc.addElement(new Double(d1-d2));
                 }
             }
 
             for(int i = 0; i < v1.size();i++){
                 if(i%2==1){
-                    double d1 = (Double)v2.elementAt(i).doubleValue();
-                    double d2 = (Double)v2.elementAt(i-1).doubleValue();
-                    v1Proc.addElement(d1-d2);
+                    double d1 = ((Double)v2.elementAt(i)).doubleValue();
+                    double d2 = ((Double)v2.elementAt(i-1)).doubleValue();
+                    v1Proc.addElement(new Double(d1-d2));
                     if(v1Proc.size()==v2Proc.size()){
                         break;
                     }
@@ -56,8 +63,8 @@ class DataProc{
             PrintWriter out1 = new PrintWriter(new BufferedWriter(new FileWriter("timeRecords.1.1.1.128.txt")));
             PrintWriter out2 = new PrintWriter(new BufferedWriter(new FileWriter("timeRecords.1.2.2.128.txt")));
             for(int i=0; i<v2Proc.size();i++){
-                out1.println((Double)v1Proc.elementAt(i).doubleValue());
-                out2.println((Double)v2Proc.elementAt(i).doubleValue());
+                out1.println(((Double)v1Proc.elementAt(i)).doubleValue());
+                out2.println(((Double)v2Proc.elementAt(i)).doubleValue());
             }
             out1.flush();
             out1.close();
