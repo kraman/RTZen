b8b91d76268fd1c7475a3419e7f91a21fe5484a8
Yue Zhang <yuez@doc.ece.uci.edu>
2005-02-22 06:52:57 +0000

    Added calls to set cpu affinity  for the Java VM and also disable IRQ's on that processor. This helps to minimize jitter problems due to I/O
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@1975 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/jpl/mackinac_server.sh
+++ packages/demo/jpl/mackinac_server.sh
@@ -1,4 +1,11 @@
 export RTZEN_HOME=/home/yuez/RTZen_newrepo
 
-java -verbose -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
+psradm -i 0
+psrset -d 1
+psrset -c
+psrset -a 1 0
+
+#java -verbose -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
+
+psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
 
