72bb2e985cdbbb38b7a24e9349699a5a127fd235
Mark Panahi <mpanahi@doc.ece.uci.edu>
2005-03-15 01:03:01 +0000

    Fixed a synch problem. Also exanded mem when running jpl client.
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2894 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/src/edu/uci/ece/zen/orb/RTCurrent.java
+++ packages/src/edu/uci/ece/zen/orb/RTCurrent.java
@@ -4,7 +4,7 @@ import edu.uci.ece.zen.utils.IntHashtable;
 import javax.realtime.ImmortalMemory;
 /**
  * Interface definition : Current
- * 
+ *
  * @author OpenORB Compiler
  */
 public class RTCurrent extends org.omg.CORBA.LocalObject implements
@@ -12,23 +12,23 @@ public class RTCurrent extends org.omg.CORBA.LocalObject implements
 
     private static IntHashtable hash;
     private static RTCurrent instance;
-            
+
     //short priority;
 
     //ORB orb;
 
-    public static RTCurrent instance(){
+    public static synchronized RTCurrent instance(){
         if(instance == null){
             try{
                 instance = (RTCurrent) (ImmortalMemory.instance().newInstance(RTCurrent.class));
                 instance.init();
             }catch(Exception e){
                 e.printStackTrace();//TODO better error handling
-            }            
+            }
         }
         return instance;
-    }    
-    
+    }
+
     private void init() {
         //orbMemoryArea = RealtimeThread.getCurrentMemoryArea();
         //this.orb = orb;
@@ -36,13 +36,13 @@ public class RTCurrent extends org.omg.CORBA.LocalObject implements
             hash = (IntHashtable) ImmortalMemory.instance().newInstance(IntHashtable.class);
         }catch(Exception e){
             e.printStackTrace();//TODO
-        }        
+        }
         hash.init(1023);
     }
 
     /**
      * Read accessor for the_priority attribute
-     * 
+     *
      * @return the attribute value
      */
     public short the_priority() {
@@ -51,7 +51,7 @@ public class RTCurrent extends org.omg.CORBA.LocalObject implements
 
     /**
      * Write accessor for the_priority attribute
-     * 
+     *
      * @param value
      *            the attribute value
      */
