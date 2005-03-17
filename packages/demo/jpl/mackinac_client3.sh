7fc579ca8c226d6f1a73f883641b888491ac4111
Krishna Raman <kraman@gmail.com>
2005-03-17 06:18:15 +0000

    Increased the initial Immortal memory size and Scope memory size
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2925 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/jpl/mackinac_client3.sh
+++ packages/demo/jpl/mackinac_client3.sh
@@ -5,6 +5,6 @@ psrset -d 1
 psrset -c
 psrset -a 1 0
 
-psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=100M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes -XX:+RTSJIgnoreThrowBoundaryError demo.jpl.Client3 
+psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=200M -classpath $RTZEN_HOME/classes -XX:+RTSJIgnoreThrowBoundaryError demo.jpl.Client3 
 #java -verbose -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=100M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Client3
 
