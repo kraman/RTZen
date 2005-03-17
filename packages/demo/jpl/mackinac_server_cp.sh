0a92b1f5ab4c3647225a1424317ffd71f3f3c696
Krishna Raman <kraman@gmail.com>
2005-03-17 05:45:17 +0000

    Changed the running scripts for both ServerDeclared and ClientPropogated
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2923 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/demo/jpl/mackinac_server_cp.sh
@@ -0,0 +1,11 @@
+export RTZEN_HOME=/home/yuez/RTZen
+
+psradm -i 0
+psrset -d 1
+psrset -c
+psrset -a 1 0
+
+#java -verbose -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
+
+psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=300M -XX:ScopedSize=200M -XX:+RTSJIgnoreThrowBoundaryError -classpath $RTZEN_HOME/classes demo.jpl.Server cp 
+
