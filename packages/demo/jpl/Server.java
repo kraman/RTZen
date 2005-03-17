5457a743e6efd0e66a205d7fa83ee1df5c276a85
Mark Panahi <mpanahi@doc.ece.uci.edu>
2005-03-17 02:02:47 +0000

    Removed dynamic thread creation.
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2918 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/jpl/Server.java
+++ packages/demo/jpl/Server.java
@@ -105,14 +105,23 @@ public class Server extends RealtimeThread
                         (short)0);
                         
                 ThreadpoolLane[] lanes = new ThreadpoolLane[2];
-                lanes[0] = new ThreadpoolLane(minPriority.value, 1, 1);
-                lanes[1] = new ThreadpoolLane(maxPriority.value, 1, 1);
+                lanes[0] = new ThreadpoolLane(minPriority.value, 5, 0);
+                lanes[1] = new ThreadpoolLane(maxPriority.value, 5, 0);
                     
                 threadPoolId = rtorb.create_threadpool_with_lanes(10, lanes, false, false, 10, 10);
                 policy[1] = rtorb.create_threadpool_policy(threadPoolId);
             }else{
-                System.out.println("Using server-declared policy.....");                
-                threadPoolId = rtorb.create_threadpool(10, 5, 5, maxPriority.value, false, 10, 10);            
+                System.out.println("Using server-declared policy.....");    
+                threadPoolId = rtorb.create_threadpool(
+                        0,//stacksize,
+                        5,//static_threads,
+                        0,//dynamic_threads,
+                        maxPriority.value,//default_thread_priority,
+                        false,//allow_request_buffering,
+                        0,//max_buffered_requests,
+                        0//max_request_buffer_size
+                        );                
+                //threadPoolId = rtorb.create_threadpool(10, 5, 5, maxPriority.value, false, 0, 10);            
                 policy = new Policy[1];
                 policy[0] = rtorb.create_threadpool_policy(threadPoolId);                
             }
