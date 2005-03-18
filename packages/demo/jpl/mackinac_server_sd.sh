6737fc188e41e52effa407299e072cc3235673e5
Yue Zhang <yuez@doc.ece.uci.edu>
2005-03-18 20:46:59 +0000

    Changed the running scripts to adapt the commands line changes in code.
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2949 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/demo/jpl/mackinac_server_sd.sh
+++ packages/demo/jpl/mackinac_server_sd.sh
@@ -7,5 +7,5 @@ psrset -a 1 0
 
 #java -verbose -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Server
 
-psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=300M -XX:ScopedSize=200M -XX:+RTSJIgnoreThrowBoundaryError -classpath $RTZEN_HOME/classes demo.jpl.Server sd 
+psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=300M -XX:ScopedSize=200M -XX:+RTSJIgnoreThrowBoundaryError -classpath $RTZEN_HOME/classes demo.jpl.Server -pm sd -hp 10000 -st 5 
 
