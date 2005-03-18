2fcde7dd180b4cf3bb67392fcfa626eb271e0ff8
Mark Panahi <mpanahi@doc.ece.uci.edu>
2005-03-18 22:45:33 +0000

    Added license info
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2952 bfff1265-3dd4-0310-90a8-ce2828190deb

--- packages/util/serial_tester/SimpleWrite.java
+++ packages/util/serial_tester/SimpleWrite.java
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) 2005 by the University of California, Irvine
+ * All Rights Reserved.
+ * 
+ * This software is released under the terms of the RTZen license, which
+ * you should have received along with this software. If not, you may
+ * obtain a copy here: http://zen.ece.uci.edu/rtzen/license.php
+ */
+
+/*
  * @(#)SimpleWrite.java 1.12 98/06/25 SMI
  * 
  * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
