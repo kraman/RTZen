27fbfb9618c10d7c3381c795ac91d62d58dd2aef
Krishna Raman <kraman@gmail.com>
2005-03-27 08:29:30 +0000

    Added the running script for Gunar's test on mackinac
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@2982 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/perf/jitter/mackinac_client.sh
@@ -0,0 +1,10 @@
+export RTZEN_HOME=/home/yuez/RTZen
+
+psradm -i 0
+psrset -d 1
+psrset -c
+psrset -a 1 0
+
+psrset -e 1 java -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=200M -XX:ScopedSize=200M -classpath $RTZEN_HOME/classes -XX:+RTSJIgnoreThrowBoundaryError perf.jitter.Client 
+#java -verbose -Xint -Xbootclasspath/p:/$RTZEN_HOME/classes:/opt/mackinac/binaries/jre/lib/rt2.jar -Dorg.omg.CORBA.ORB=edu.uci.ece.zen.ORB -Dorg.omg.CORBA.ORBSingleton=edu.uci.ece.zen.ORBSingleton -XX:ImmortalSize=100M -XX:ScopedSize=100M -classpath $RTZEN_HOME/classes demo.jpl.Client3
+
