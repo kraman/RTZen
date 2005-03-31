92b744a77fb4519ef728af14b85ad25a7a250699
Yue Zhang <yuez@doc.ece.uci.edu>
2005-03-31 01:56:16 +0000

    Add script to compile the tao jpl test
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTZen/trunk@3004 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ packages/perf/jitter/tao_jpl/compile.sh
@@ -0,0 +1,8 @@
+make -f GNUmakefile.RTCORBA_Server_Declared_Server clean
+make -f GNUmakefile.RTCORBA_Server_Declared_Client1 clean
+make -f GNUmakefile.RTCORBA_Server_Declared_Client2 clean
+
+make -f GNUmakefile.RTCORBA_Server_Declared_Server
+make -f GNUmakefile.RTCORBA_Server_Declared_Client1
+make -f GNUmakefile.RTCORBA_Server_Declared_Client2
+
